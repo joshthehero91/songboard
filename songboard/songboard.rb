e60331c73c2eb843c083975ff14591b7541a432e
joshthehero91 <joshthehero91@gmail.com>
2021-03-29 14:12:00 -0400

    output to json

--- songboard/songboard.rb
+++ songboard/songboard.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Song                                   # create the Song class
   attr_accessor :title, :album, :bpm         # require title, album, and BPM
 end
@@ -14,9 +16,27 @@ def add_song()                               # function to add a new song to mus
   s.title = title                            # set title
   s.album = album                            # set album
   s.bpm   = bpm                              # set bpm
+   
+  song1 = { "title" => s.title, "album" => s.album, "bpm" => s.bpm  }
+
   puts "TITLE: " + s.title                   # verify title
   puts "ALBUM: " + s.album                   # verify album
   puts "BPM:   " + s.bpm                     # verify BPM
+  
+  puts "Is this correct? (y/n)"
+  answer = gets.chomp().downcase.to_s
+  if answer == "y"
+    puts "Writing to file..."
+    File.write("library.json",song1.to_json)
+    puts ""
+  elsif answer == "n"
+    puts "Please try again."
+    puts ""
+    add_song()
+  else
+    puts "Please enter y or n."
+    puts ""
+  end
 end
 
 add_song()                                   # run to add a new song
