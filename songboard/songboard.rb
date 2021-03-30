51580c801e96220cc5ac08c7aef6985c444ce1aa
joshthehero91 <joshthehero91@gmail.com>
2021-03-30 17:59:37 -0400

    added delete_song() and backed up library

--- songboard/songboard.rb
+++ songboard/songboard.rb
@@ -44,7 +44,7 @@ def add_song                                                    # function to ad
   answer = gets.chomp().downcase.to_s
   if 
     answer == "y"                                               # if everything looks correct
-    puts "Writing to file..."                                   # write to library.json file
+    puts "Saving to library..."                                   # write to library.json file
     File.write("library.json",library.to_json)                 
     puts ""
   elsif 
@@ -88,17 +88,14 @@ def delete_song
     file = File.read("library.json")                            # open file
     library = JSON.parse(file)                                  # read file    
   end
-  
-  puts """
-  Please provide the name of the song being removed
-  or use \"back\" to return to the main menu:
-  """
+
+  puts "Please provide the name of the song being removed or use \"back\" to return to the main menu: "
   choice = gets.chomp().to_s
-  
+
   if 
     choice == "back"
     main_menu()
-  else
+  elsif
     library.each do |track, song| 
       if
         song.has_value? choice == false
@@ -107,14 +104,38 @@ def delete_song
         song.has_value? choice
         title, album, bpm = song.values
         puts "Is this the song you would like to remove?"    
-        puts "|------------------------------------------------"
-        puts "| TITLE: " + title.to_s
-        puts "| ALBUM: " + album.to_s
-        puts "| BPM:   " + bpm.to_s
-        puts "|------------------------------------------------"
-      end 
+        puts ""
+        puts "           |------------------------------------------------"
+        puts "           | TITLE: " + title.to_s
+        puts "           | ALBUM: " + album.to_s
+        puts "           | BPM:   " + bpm.to_s
+        puts "           |------------------------------------------------"
+        puts ""
+        
+        puts "Is this correct? (y/n)"
+        answer = gets.chomp().downcase.to_s
+        if
+          
+          answer == "y"                                         # if everything looks correct 
+          library.delete(track)
+        elsif
+          answer == "n"                                               # or allow user to retry
+          puts "Please try again."
+          puts ""
+          delete_song()
+        else                                                          # otherwise verify it's only "y" or "n"
+          puts "Please enter y or n."
+          puts ""
+        end
+      end
     end
+  puts "Removing from library..."                                   # write to library.json file
+  File.write("library.json",library.to_json)
+  puts ""
+  list_songs() 
   end
+  puts "No Results found. Please try again."
+  puts ""
   main_menu()
 end
 
